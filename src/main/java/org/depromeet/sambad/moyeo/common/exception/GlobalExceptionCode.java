539d7a48a210d33b3085952b49a2fc33dbaa575b
nahyeon99 <nahyeonee99@gmail.com>
2024-07-10 18:51:48 +0900

    style: static import 로 수정

--- src/main/java/org/depromeet/sambad/moyeo/common/exception/GlobalExceptionCode.java
+++ src/main/java/org/depromeet/sambad/moyeo/common/exception/GlobalExceptionCode.java
@@ -1,5 +1,7 @@
 package org.depromeet.sambad.moyeo.common.exception;
 
+import static org.springframework.http.HttpStatus.*;
+
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
@@ -8,9 +10,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum GlobalExceptionCode implements ExceptionCode {
-	INVALID_INPUT(HttpStatus.BAD_REQUEST, "유효한 입력 형식이 아닙니다."),
+	INVALID_INPUT(BAD_REQUEST, "유효한 입력 형식이 아닙니다."),
 
-	SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 문제가 발생했습니다.");
+	SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 문제가 발생했습니다.");
 
 	private final HttpStatus status;
 	private final String message;
