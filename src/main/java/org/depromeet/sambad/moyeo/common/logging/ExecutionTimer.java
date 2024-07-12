817450c24b92692f63b720f2b2c2e4dd2f7d9225
nahyeon99 <nahyeonee99@gmail.com>
2024-07-12 18:22:41 +0900

    feat: 로컬 환경에서 실행 메서드 로깅 구현

---
+++ src/main/java/org/depromeet/sambad/moyeo/common/logging/ExecutionTimer.java
@@ -0,0 +1,11 @@
+package org.depromeet.sambad.moyeo.common.logging;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExecutionTimer {
+}
