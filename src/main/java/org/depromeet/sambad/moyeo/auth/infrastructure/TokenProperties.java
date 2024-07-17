dce3108d948653690cb3dcf01b3ec4262e04aa29
Nahyeon Kim <nahyeonee99@gmail.com>
2024-07-17 22:32:44 +0900

    fix: filter를 AOP 타겟에서 제거 (#12)

--- src/main/java/org/depromeet/sambad/moyeo/auth/infrastructure/TokenProperties.java
+++ src/main/java/org/depromeet/sambad/moyeo/auth/infrastructure/TokenProperties.java
@@ -1,12 +1,13 @@
 package org.depromeet.sambad.moyeo.auth.infrastructure;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "jwt")
 public record TokenProperties(
-		@NotNull String secretKey,
-		@Min(0) long expirationTimeMs
+	@NotNull String secretKey,
+	@Min(0) long expirationTimeMs
 ) {
 }
--- src/main/java/org/depromeet/sambad/moyeo/meetingQuestion/domain/MeetingQuestion.java
+++ src/main/java/org/depromeet/sambad/moyeo/meetingQuestion/domain/MeetingQuestion.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.depromeet.sambad.moyeo.meeting.domain.Meeting;
 import org.depromeet.sambad.moyeo.meetingAnswer.domain.MeetingMemberAnswer;
 import org.depromeet.sambad.moyeo.question.domain.Question;
 
@@ -30,14 +29,14 @@ public class MeetingQuestion {
 	private Long id;
 
 	// FIXME: Meeting에서 mappedBy = "meeting" 지정
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "meeting_id")
-	private Meeting meeting;
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "meeting_id")
+	// private Meeting meeting;
 
 	// FIXME: MeetingMember에서 mappedBy="registeredMember" 지정
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "meeting_member_id")
-	private MeetingMember registeredMember;
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "meeting_member_id")
+	// private MeetingMember registeredMember;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "question_id")
