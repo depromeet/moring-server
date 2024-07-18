acbe816f21c73e51db96e7f65c737105e0946860
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-07-19 02:46:47 +0900

    [SAMBAD-95]-feat: object storage config 설정

---
+++ src/main/java/org/depromeet/sambad/moring/file/infrastructure/ObjectStorageProperties.java
@@ -0,0 +1,30 @@
+package org.depromeet.sambad.moring.file.infrastructure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotNull;
+
+@Validated
+@ConfigurationProperties(prefix = "cloud.ncp")
+public record ObjectStorageProperties(
+	@NotNull ObjectStorage objectStorage,
+	@NotNull Region region
+) {
+
+	public record ObjectStorage(
+		@NotNull String endpoint,
+		@NotNull Credentials credentials) {
+	}
+
+	public record Credentials(
+		@NotNull String accessKey,
+		@NotNull String secretKey,
+		@NotNull String bucket) {
+	}
+
+	public record Region(
+		@NotNull String staticRegion,
+		boolean auto) {
+	}
+}
