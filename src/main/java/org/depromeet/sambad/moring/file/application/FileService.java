690c98af5b0b1ca7522b6bbbfdb7ebb08b788b83
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-07-19 02:46:59 +0900

    [SAMBAD-95]-feat: 파일 업로드 구현

--- src/main/java/org/depromeet/sambad/moring/file/application/FileService.java
+++ src/main/java/org/depromeet/sambad/moring/file/application/FileService.java
@@ -1,9 +1,14 @@
 package org.depromeet.sambad.moring.file.application;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.depromeet.sambad.moring.file.domain.FileEntity;
 import org.depromeet.sambad.moring.file.domain.FileRepository;
+import org.depromeet.sambad.moring.file.infrastructure.ObjectStorageFileUploader;
+import org.depromeet.sambad.moring.file.presentation.exception.NotFoundFileException;
+import org.depromeet.sambad.moring.file.presentation.response.FileUrlResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -13,6 +18,24 @@ public class FileService {
 
 	private final FileUploader fileUploader;
 	private final FileRepository fileRepository;
+	private final ObjectStorageFileUploader objectStorageFileUploader;
+
+	@Transactional
+	public FileUrlResponse upload(MultipartFile multipartFile) throws IOException {
+		String url = objectStorageFileUploader.upload(multipartFile, multipartFile.getOriginalFilename());
+		FileEntity fileEntity = FileEntity.of(multipartFile.getOriginalFilename(), url);
+		fileRepository.save(fileEntity);
+		return FileUrlResponse.of(url);
+	}
+
+	@Transactional
+	public void delete(Long fileId) {
+		if(isNotExistFile(fileId)) {
+			throw new NotFoundFileException();
+		}
+		fileRepository.deleteById(fileId);
+		objectStorageFileUploader.delete(fileId);
+	}
 
 	public FileEntity uploadAndSave(String fileUrl) {
 		// TODO: add url validation
@@ -23,5 +46,9 @@ public class FileService {
 		} catch (IOException e) {
 			throw new RuntimeException("Failed to upload file", e);
 		}
+	}
+
+	private boolean isNotExistFile(Long fileId) {
+		return !fileRepository.existsById(fileId);
 	}
 }
--- src/main/java/org/depromeet/sambad/moring/file/infrastructure/FileRepositoryImpl.java
+++ src/main/java/org/depromeet/sambad/moring/file/infrastructure/FileRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.depromeet.sambad.moring.file.infrastructure;
 import lombok.RequiredArgsConstructor;
 import org.depromeet.sambad.moring.file.domain.FileEntity;
 import org.depromeet.sambad.moring.file.domain.FileRepository;
+import org.depromeet.sambad.moring.file.presentation.exception.NotFoundFileException;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -13,5 +14,21 @@ public class FileRepositoryImpl implements FileRepository {
 	@Override
 	public FileEntity save(FileEntity fileEntity) {
 		return fileJpaRepository.save(fileEntity);
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		return fileJpaRepository.existsById(id);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		fileJpaRepository.deleteById(id);
+	}
+
+	@Override
+	public FileEntity findById(Long id) {
+		return fileJpaRepository.findById(id)
+			.orElseThrow(NotFoundFileException::new);
 	}
 }
