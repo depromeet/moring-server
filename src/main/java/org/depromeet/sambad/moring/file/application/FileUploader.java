54cb437a67d2042a9048ab6a959b1a1b34108253
nahyeon99 <nahyeonee99@gmail.com>
2024-07-19 15:35:02 +0900

    fix: FileUploader Profile 설정 및 구현체 의존 문제 수정

--- src/main/java/org/depromeet/sambad/moring/file/application/FileUploader.java
+++ src/main/java/org/depromeet/sambad/moring/file/application/FileUploader.java
@@ -1,15 +1,17 @@
 package org.depromeet.sambad.moring.file.application;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
 
 public interface FileUploader {
 
 	String upload(MultipartFile file, String originalFileName) throws IOException;
 
 	String upload(String fileUrl) throws IOException;
+
+	void delete(Long Id);
 
 	default String getFileExtension(String fileName) {
 		int dotIndex = fileName.lastIndexOf('.');
