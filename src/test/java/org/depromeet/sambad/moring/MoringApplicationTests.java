6e77f7c56584c6c2d116fe402e853c23a9fadf71
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-07-25 16:54:07 +0900

    [SAMBAD-158]-fix : ./gradlew build jacocoTestRepost 로 변경

--- src/test/java/org/depromeet/sambad/moring/MoringApplicationTests.java
+++ src/test/java/org/depromeet/sambad/moring/MoringApplicationTests.java
@@ -3,11 +3,11 @@ package org.depromeet.sambad.moring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// class MoringApplicationTests {
-//
-// 	@Test
-// 	void contextLoads() {
-// 	}
-//
-// }
+@SpringBootTest
+class MoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
