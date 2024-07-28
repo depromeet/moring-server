a53fa740ed08587f71c3d9228e03f482f1009957
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-07-28 21:34:56 +0900

    [SAMBAD-149]-feat: sql 데이터 추가

---
+++ src/main/java/org/depromeet/sambad/moring/common/application/DataInitializationService.java
@@ -0,0 +1,8 @@
+package org.depromeet.sambad.moring.common.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataInitializationService {
+
+}
---
+++ src/main/resources/db/meeting.sql
@@ -0,0 +1,1 @@
+insert into meeting (meeting_id, code, name, created_at, updated_at) values (1, '0AF781', '반갑다 친구들아', '2024-03-08 07:43:34', '2023-12-20 05:19:49');
