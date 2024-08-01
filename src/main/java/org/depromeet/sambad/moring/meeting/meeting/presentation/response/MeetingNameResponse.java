3d4f4a37076af4188a454f8e14bf9bf359ad4d4e
Kijun Kwon <kkjsw17@naver.com>
2024-08-02 01:34:21 +0900

    [SAMBAD-204] Response 필드명 변경 및 설명 추가

--- src/main/java/org/depromeet/sambad/moring/meeting/meeting/presentation/response/MeetingNameResponse.java
+++ src/main/java/org/depromeet/sambad/moring/meeting/meeting/presentation/response/MeetingNameResponse.java
@@ -1,9 +1,14 @@
 package org.depromeet.sambad.moring.meeting.meeting.presentation.response;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.*;
+
 import org.depromeet.sambad.moring.meeting.meeting.domain.Meeting;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record MeetingNameResponse(
-	String title
+	@Schema(description = "모임명", example = "삼밧드의 모험", requiredMode = REQUIRED)
+	String name
 ) {
 	public static MeetingNameResponse from(Meeting meeting) {
 		return new MeetingNameResponse(meeting.getName());
