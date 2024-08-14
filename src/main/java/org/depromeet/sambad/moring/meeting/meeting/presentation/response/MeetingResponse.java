ac4624ddbeb5c3751ad449ae18103ba148108813
Kijun Kwon <kkjsw17@naver.com>
2024-08-15 00:35:35 +0900

    [SAMBAD-248] 모임 목록 API에 모임명 정보 추가

--- src/main/java/org/depromeet/sambad/moring/meeting/meeting/presentation/response/MeetingResponse.java
+++ src/main/java/org/depromeet/sambad/moring/meeting/meeting/presentation/response/MeetingResponse.java
@@ -9,14 +9,23 @@ import org.depromeet.sambad.moring.meeting.meeting.domain.Meeting;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MeetingResponse(
-	@Schema(description = "모임 ID 목록", example = "[1, 2, 3]", requiredMode = REQUIRED)
-	List<Long> meetingIds
+	@Schema(description = "가입 되어 있는 모임의 정보 목록", requiredMode = REQUIRED)
+	List<MeetingResponseDetail> meetings
 ) {
 	public static MeetingResponse from(List<Meeting> meetings) {
 		return new MeetingResponse(
 			meetings.stream()
-				.map(Meeting::getId)
+				.map(meeting -> new MeetingResponseDetail(meeting.getId(), meeting.getName()))
 				.toList()
 		);
+	}
+
+	public record MeetingResponseDetail(
+		@Schema(description = "모임 ID", example = "1", requiredMode = REQUIRED)
+		Long meetingId,
+
+		@Schema(description = "모임명", example = "1", requiredMode = REQUIRED)
+		String name
+	) {
 	}
 }
