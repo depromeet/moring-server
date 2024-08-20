b3edc6301b0bb2b5f4a2a7eaf80c0fbe42eed345
Kijun Kwon <kkjsw17@naver.com>
2024-08-20 22:29:36 +0900

    [SAMBAD-268] P6spyConfig 관련 운영 오류 수정

--- src/main/java/org/depromeet/sambad/moring/common/config/P6spyConfig.java
+++ src/main/java/org/depromeet/sambad/moring/common/config/P6spyConfig.java
@@ -2,21 +2,32 @@ package org.depromeet.sambad.moring.common.config;
 
 import org.depromeet.sambad.moring.common.logging.P6spySqlLoggingFormatter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import com.p6spy.engine.logging.P6LogLoadableOptions;
 import com.p6spy.engine.logging.P6LogOptions;
+import com.p6spy.engine.spy.P6SpyLoadableOptions;
 import com.p6spy.engine.spy.P6SpyOptions;
 
 import jakarta.annotation.PostConstruct;
 
+@Profile({"local", "dev"})
 @Configuration
 public class P6spyConfig {
 
 	@PostConstruct
 	public void setLogMessageFormat() {
-		P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlLoggingFormatter.class.getName());
+		P6SpyLoadableOptions spyActiveInstance = P6SpyOptions.getActiveInstance();
+		P6LogLoadableOptions logActiveInstance = P6LogOptions.getActiveInstance();
+
+		if (spyActiveInstance == null || logActiveInstance == null) {
+			return;
+		}
+
+		spyActiveInstance.setLogMessageFormat(P6spySqlLoggingFormatter.class.getName());
 
 		// exclude sql pattern
-		P6LogOptions.getActiveInstance().setFilter(true);
-		P6LogOptions.getActiveInstance().setExclude("shedlock");
+		logActiveInstance.setFilter(true);
+		logActiveInstance.setExclude("shedlock");
 	}
 }
