58f80243b299574d5eb96c14277c6c5e1f67208c
이한음 <leehaneum160924@kyonggi.ac.kr>
2024-08-21 22:32:22 +0900

    [SAMBAD-273]-손 흔들어 인사하기 API (#119)
    * [SAMBAD-273]-feat: 손 흔들어 인사하기 API 구현
    
    * [SAMBAD-273]-feat: 이미 ACCEPTED REJECTED 일 때 예외처리
    
    * [SAMBAD-273]-refactor: 예외처리 ApiResponse에 추가
    
    * [SAMBAD-273]-refactor: 코드 리뷰 반영
    
    * [SAMBAD-273]-refactor: validate 관련 리뷰 반영
    
    * [SAMBAD-273]-refactor: 자잘한 수정
    
    * [SAMBAD-273]-refactor: receiver validation 추가
    
    * [SAMBAD-273]-refactor: wavings 복수형으로 엔드포인트 수정

--- src/main/java/org/depromeet/sambad/moring/meeting/handwaving/infrastructure/HandWavingJpaRepository.java
+++ src/main/java/org/depromeet/sambad/moring/meeting/handwaving/infrastructure/HandWavingJpaRepository.java
@@ -1,6 +1,6 @@
-package org.depromeet.sambad.moring.meeting.handWaving.infrastructure;
+package org.depromeet.sambad.moring.meeting.handwaving.infrastructure;
 
-import org.depromeet.sambad.moring.meeting.handWaving.domain.HandWaving;
+import org.depromeet.sambad.moring.meeting.handwaving.domain.HandWaving;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HandWavingJpaRepository extends JpaRepository<HandWaving, Long> {
