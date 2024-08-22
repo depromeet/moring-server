67393d682797bf86f0275b5e4139214611361700
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2024-08-22 17:40:01 +0900

    [SAMBAD-275] 모임 코드 조회 API 추가 (#124)

---
+++ src/main/java/org/depromeet/sambad/moring/meeting/meeting/presentation/response/MeetingCodeResponse.java
@@ -0,0 +1,16 @@
+package org.depromeet.sambad.moring.meeting.meeting.presentation.response;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.*;
+
+import org.depromeet.sambad.moring.meeting.meeting.domain.Meeting;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record MeetingCodeResponse(
+	@Schema(description = "초대 코드", example = "ABCDEF", requiredMode = REQUIRED)
+	String code
+) {
+	public static MeetingCodeResponse from(Meeting meeting) {
+		return new MeetingCodeResponse(meeting.getCode());
+	}
+}
