180787d8e38ae649666a0da2114f75d12e379840
nahyeon99 <nahyeonee99@gmail.com>
2024-08-24 23:28:56 +0900

    fix: 페이징 응답 시 페이지 개수 버그 수정

--- src/main/java/org/depromeet/sambad/moring/meeting/question/presentation/response/FullInactiveMeetingQuestionListResponse.java
+++ src/main/java/org/depromeet/sambad/moring/meeting/question/presentation/response/FullInactiveMeetingQuestionListResponse.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.depromeet.sambad.moring.common.response.PageableResponse;
 import org.depromeet.sambad.moring.meeting.question.domain.MeetingQuestion;
-import org.springframework.data.domain.Pageable;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -22,11 +21,10 @@ public record FullInactiveMeetingQuestionListResponse<T>(
 ) {
 
 	public static FullInactiveMeetingQuestionListResponse of(List<MeetingQuestion> meetingQuestions,
-		Pageable pageable) {
+		PageableResponse pageableResponse) {
 		List<FullInactiveMeetingQuestionListResponseDetail> responseDetails = FullInactiveMeetingQuestionListResponseDetail.from(
 			meetingQuestions);
 
-		return new FullInactiveMeetingQuestionListResponse(responseDetails,
-			PageableResponse.of(pageable, responseDetails));
+		return new FullInactiveMeetingQuestionListResponse(responseDetails, pageableResponse);
 	}
 }
