825e26717c7b6600f43004779409971a70809f94
이한음 <leehaneum160924@kyonggi.ac.kr>
2024-08-28 15:19:17 +0900

    [SAMBAD-309]-손흔들기 수락 거절 시 이벤트 inactive (#148)

--- src/main/java/org/depromeet/sambad/moring/meeting/handwaving/application/HandWavingService.java
+++ src/main/java/org/depromeet/sambad/moring/meeting/handwaving/application/HandWavingService.java
@@ -56,6 +56,7 @@ public class HandWavingService {
 		HandWaving handWaving = getHandWavingById(handWavingId);
 		handWaving.validateIsReceiver(userId);
 		handWaving.accept();
+		eventService.inactivateLastEventByType(userId, meetingId, HAND_WAVING_REQUESTED);
 	}
 
 	@Transactional
@@ -64,6 +65,7 @@ public class HandWavingService {
 		HandWaving handWaving = getHandWavingById(handWavingId);
 		handWaving.validateIsReceiver(userId);
 		handWaving.reject();
+		eventService.inactivateLastEventByType(userId, meetingId, HAND_WAVING_REQUESTED);
 	}
 
 	public List<HandWavingSummary> getHandWavingSummariesBy(List<Event> events) {
